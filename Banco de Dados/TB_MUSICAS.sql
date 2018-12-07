c33e75e03b05c47723bc03e2b242a7764b0f1c44
caireserison <erisoncaires@gmail.com>
2018-12-07 00:41:13 -0200

    Incluindo a estrutura de dados de música por usuário

--- Banco de Dados/TB_MUSICAS.sql
+++ Banco de Dados/TB_MUSICAS.sql
@@ -1,8 +1,6 @@
-﻿
-CREATE TABLE TB_MUSICAS(
-	[idUsuario] INTEGER NOT NULL
-	[idFaixa] INTEGER NOT NULL
-	[dtInclusao] DATETIME NOT NULL
+﻿CREATE TABLE TB_MUSICAS(
+	[idUsuario] INTEGER NOT NULL,
+	[idFaixa] INTEGER NOT NULL,
+	[dtInclusao] DATETIME NOT NULL,
 	PRIMARY KEY(idUsuario,idFaixa,dtInclusao)
-	)
-	
+)
---
+++ Infomusica/App_Code/Entities/Musica/Musica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de Musica
+/// </summary>
+public class Musica : IMusica
+{
+    public int idUsuario { get; set; }
+    public int idFaixa { get; set; }
+    public System.DateTime dtInclusao { get; set; }
+}
