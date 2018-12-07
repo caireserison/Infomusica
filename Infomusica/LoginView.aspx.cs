4e65044c93c8da22f1bda9e656c78645e0e11bab
caireserison <erisoncaires@gmail.com>
2018-12-06 22:11:39 -0200

    Aplicando ação de login a tela

--- Infomusica/LoginView.aspx.cs
+++ Infomusica/LoginView.aspx.cs
@@ -7,10 +7,29 @@ using System.Web.UI.WebControls;
 
 public partial class LoginView : System.Web.UI.Page
 {
+    LoginController controle = new LoginController();
+    Login login = new Login();
 
-    protected void Page_Load(object sender, EventArgs e)
+    protected void btLogin_Click(object sender, EventArgs e)
     {
+        AutenticarUsuario();
 
+        if (login.id == 0)
+        {
+            // TODO: Mensagem de usuário inexistente ou inválido
+            return;
+        }
+        
+        
+        //TODO: REDIRECT
     }
 
+    private void AutenticarUsuario()
+    {
+        login.login = tbUsuario.Text.Trim();
+        login.senha = tbSenha.Text.Trim();
+
+        login = (Login)controle.AutenticarUsuario(login);
+        Session["usuario"] = login;
+    }
 }
